2f72b9649e152834eb51e4c1d2b8e607b5dba936
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-10-12 20:06:50 +0300

    Add utf-8 support

--- src/extension.ts
+++ src/extension.ts
@@ -220,6 +220,7 @@ class RelativePath {
             window.activeTextEditor.edit(
                 (editBuilder: TextEditorEdit) => {
                     let position: Position = window.activeTextEditor.selection.end;
+                    relativeUrl = encodeURI(relativeUrl);
                     editBuilder.insert(position, relativeUrl);
                 }
             );
@@ -259,10 +260,10 @@ class RelativePath {
         // Don't filter on too many files. Show the input search box instead
         if (disableQuickFilter) {
             const placeHolder = `Found ${this._items.length} files. Enter the filter query. Consider adding more 'relativePath.ignore' settings.`;
-            const input = window.showInputBox({placeHolder});
+            const input = window.showInputBox({ placeHolder });
             input.then(val => {
                 if (val === undefined) {
-                    // User pressed 'Escape' 
+                    // User pressed 'Escape'
                     return;
                 }
 
