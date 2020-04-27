6089ad48da7701b672d70bc78503fd79c4dca629
Rapsy Tree <rapsalands@gmail.com>
2020-04-27 12:07:01 -0700

    Reverting junk changes.

--- src/extension.ts
+++ src/extension.ts
@@ -225,15 +225,8 @@ class RelativePath {
 
             window.activeTextEditor.edit(
                 (editBuilder: TextEditorEdit) => {
-                    // Get all selections
-                    let selections = window.activeTextEditor.selections;
-
-                    // Replace selections with relative Url.
-                    selections.forEach(sel => {
-                        editor.edit(editBuilder => {
-                            editBuilder.replace(sel, relativeUrl);
-                        });
-                    });
+                    let position: Position = window.activeTextEditor.selection.end;
+                    editBuilder.insert(position, relativeUrl);
                 }
             );
         }
