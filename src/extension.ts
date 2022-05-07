65a45fa74b41242df8b24e9dc3371833fbd9547b
Rapsy Tree <rapsalands@gmail.com>
2022-05-06 20:40:08 -0700

    ✨ Show dotfiles in suggestions (#46)
    Co-authored-by: daws.eth <3408480+dawsbot@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -99,7 +99,7 @@ class RelativePath {
             }
         } else {
             this._myGlob = new Glob(this._workspacePath + "/**/*.*",
-                { ignore: this._configuration.get("ignore") },
+                { ignore: this._configuration.get("ignore"), dot: true, nodir: true },
                 (err, files) => {
                     if (err) {
                         return;
@@ -259,7 +259,7 @@ class RelativePath {
         // Don't filter on too many files. Show the input search box instead
         if (disableQuickFilter) {
             const placeHolder = `Found ${this._items.length} files. Enter the filter query. Consider adding more 'relativePath.ignore' settings.`;
-            const input = window.showInputBox({placeHolder});
+            const input = window.showInputBox({ placeHolder });
             input.then(val => {
                 if (val === undefined) {
                     // User pressed 'Escape' 
